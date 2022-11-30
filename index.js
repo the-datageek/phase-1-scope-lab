daab5deec0d47f61cfc935c4aa098b471308f332
Faith Moraa <moraaf770@gmail.com>
2022-11-30 04:18:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+//upperCaseCustomerName()
+
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+}
+//setBestCustomer()
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+//overwriteBestCustomer()
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+
+const leastFavoriteCustomer = "Assignment to constant variable."
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "is bob"
+    return leastFavoriteCustomer
+}
